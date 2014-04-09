748983476c292bda6296a05b11241450851ae78d
dalvarad <dalvarad@alumnos.ubiobio.cl>
2014-04-09 19:58:06 -0300

    Boton agregado

--- Lab03/src/Ventana.java
+++ Lab03/src/Ventana.java
@@ -1,5 +1,7 @@
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Ventana {
 	public static void main(String[] args){
@@ -7,6 +9,15 @@ public class Ventana {
 		JLabel label= new JLabel();
 		label.setText("Hola");
 		frame.add(label);
+		JButton button = new JButton();
+		button.setText("Presioname!");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "PRESIONADO");
+			}
+		});
+		frame.add(button);
 		frame.setTitle("mi primera ventana");
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
